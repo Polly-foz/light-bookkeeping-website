e7eef3c50a7efcf819156b988665277ce14826a6
polly-foz <polly.foz@gmail.com>
2020-03-12 10:03:35 +0800

    deploy

---
+++ precache-manifest.200fe3243a10fdba1359a6d4da4e19c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd2b118be4d2a42bc1e",
+    "url": "/light-bookkeeping-website/css/app.f6d6e596.css"
+  },
+  {
+    "revision": "64527dbe45b19fc53cda",
+    "url": "/light-bookkeeping-website/css/chunk-vendors.6e491b03.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/light-bookkeeping-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/light-bookkeeping-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "5f30b0e203458635f4269b9bf6030c6e",
+    "url": "/light-bookkeeping-website/index.html"
+  },
+  {
+    "revision": "4fd2b118be4d2a42bc1e",
+    "url": "/light-bookkeeping-website/js/app.72a87bd1.js"
+  },
+  {
+    "revision": "64527dbe45b19fc53cda",
+    "url": "/light-bookkeeping-website/js/chunk-vendors.d5c389ff.js"
+  },
+  {
+    "revision": "afcd6ed3dd30b92b419ca1386674c57f",
+    "url": "/light-bookkeeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/light-bookkeeping-website/robots.txt"
+  }
+]);
